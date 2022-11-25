85d8e900c6ec5cc9ce08f81e97c414ddc2bba892
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2022-11-25 08:01:26 -0600

    chore: add Vercel CI

--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,51 @@
 import * as path from 'node:path'
 import { defineConfig } from 'vite'
 
-export default defineConfig((env) => ({
-  root: env.command === 'serve' ? 'demo' : undefined,
-  resolve: {
-    alias: {
-      four: path.resolve(process.cwd(), 'src'),
-    },
-  },
-  build: {
-    minify: 'terser',
-    sourcemap: true,
-    target: 'esnext',
-    lib: {
-      formats: ['es', 'cjs'],
-      entry: 'src/index.ts',
-      fileName: (format) => (format === 'es' ? 'index.mjs' : 'index.js'),
-    },
-    rollupOptions: {
-      output: {
-        sourcemapExcludeSources: true,
+const dev = process.env.VERCEL_CI || !process.argv[2]
+
+export default defineConfig(
+  dev
+    ? {
+        root: 'demo',
+        resolve: {
+          alias: {
+            four: path.resolve(process.cwd(), 'src'),
+          },
+        },
+        build: {
+          minify: false,
+          sourcemap: true,
+          polyfillModulePreload: false,
+        },
+      }
+    : {
+        build: {
+          minify: 'terser',
+          sourcemap: true,
+          target: 'esnext',
+          lib: {
+            formats: ['es', 'cjs'],
+            entry: 'src/index.ts',
+            fileName: (format) => (format === 'es' ? 'index.mjs' : 'index.js'),
+          },
+          rollupOptions: {
+            output: {
+              sourcemapExcludeSources: true,
+            },
+          },
+          terserOptions: {
+            safari10: false,
+            compress: true,
+            mangle: true,
+          },
+        },
+        plugins: [
+          {
+            name: 'vite-tsc',
+            generateBundle() {
+              this.emitFile({ type: 'asset', fileName: 'index.d.ts', source: `export * from '../src'` })
+            },
+          },
+        ],
       },
-    },
-    terserOptions: {
-      safari10: false,
-      compress: true,
-      mangle: true,
-    },
-  },
-  plugins: [
-    {
-      name: 'vite-tsc',
-      generateBundle() {
-        this.emitFile({ type: 'asset', fileName: 'index.d.ts', source: `export * from '../src'` })
-      },
-    },
-  ],
-}))
+)
