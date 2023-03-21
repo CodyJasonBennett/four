a8b08029358b561873a5d95f3dcc04fe8d18e48c
Cody Bennett <hi@codyb.co>
2023-03-21 15:49:56 -0500

    chore: add geometry compute example

--- examples/webgl-compute.ts
+++ examples/webgl-compute.ts
@@ -1,30 +1,68 @@
 import { WebGLRenderer, Geometry, Material, Mesh } from 'four'
 
 const renderer = new WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.canvas)
 
 const geometry = new Geometry({
-  source: {
-    data: new Float32Array([0, 1, 2, 3, 4]),
-    size: 1,
-  },
-  result: {
-    data: new Float32Array(5),
-    size: 1,
-  },
+  position: { size: 2, data: new Float32Array(6) },
+  uv: { size: 2, data: new Float32Array(6) },
 })
 
-const material = new Material({
+const computeMaterial = new Material({
   compute: /* glsl */ `#version 300 es
-    in float source;
-    out float result;
+    out vec2 position;
+    out vec2 uv;
+
+    const vec2 vertex[3] = vec2[](vec2(-1), vec2(3, -1), vec2(-1, 3));
+
     void main() {
-      result = source + float(gl_VertexID);
+      position = vertex[gl_VertexID];
+      uv = abs(position) - 1.0;
     }
   `,
 })
 
-const mesh = new Mesh(geometry, material)
+const mesh = new Mesh(geometry, computeMaterial)
 
 renderer.compute(mesh)
 
-console.log(geometry.attributes.result.data)
+mesh.material = new Material({
+  uniforms: {
+    time: 0,
+  },
+  vertex: /* glsl */ `#version 300 es
+    in vec2 uv;
+    in vec3 position;
+
+    out vec2 vUv;
+  
+    void main() {
+      vUv = uv;
+      gl_Position = vec4(position, 1);
+    }
+  `,
+  fragment: /* glsl */ `#version 300 es
+    precision highp float;
+
+    uniform float time;
+
+    in vec2 vUv;
+    out vec4 pc_fragColor;
+
+    void main() {
+      pc_fragColor = vec4(vec3(.8, .7, 1) + .3 * cos(vUv.xyx + time), 1);
+    }
+  `,
+})
+
+window.addEventListener('resize', () => {
+  renderer.setSize(window.innerWidth, window.innerHeight)
+})
+
+function animate(time: DOMHighResTimeStamp) {
+  requestAnimationFrame(animate)
+  mesh.material.uniforms.time = time / 1000
+  renderer.render(mesh)
+}
+requestAnimationFrame(animate)
