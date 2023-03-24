85ce2bb5b9cb1fb3334fba2760502839dd7fc33a
Cody Bennett <hi@codyb.co>
2023-03-24 08:16:44 -0500

    chore: don't measure with brotli

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,4 @@
-import * as fs from 'node:fs'
 import * as path from 'node:path'
-import * as zlib from 'node:zlib'
 import * as vite from 'vite'
 
 const mangleMap: Record<string, string> = {
@@ -17,14 +15,12 @@ const mangleMap: Record<string, string> = {
 
 export default vite.defineConfig({
   root: process.argv[2] ? undefined : 'examples',
-  logLevel: process.argv[2] ? 'warn' : undefined,
   resolve: {
     alias: {
       four: path.resolve(__dirname, 'src'),
     },
   },
   build: {
-    reportCompressedSize: false,
     sourcemap: true,
     target: 'es2018',
     lib: {
@@ -61,28 +57,6 @@ export default vite.defineConfig({
         handler(code, { fileName }) {
           return vite.transformWithEsbuild(code, fileName, { minify: true, target: 'es2018' })
         },
-      },
-      closeBundle() {
-        const dist = path.resolve(__dirname, 'dist')
-        const bundles: Record<string, number> = {}
-
-        for (const file of fs.readdirSync(dist)) {
-          if (file.endsWith('js')) {
-            const code = fs.readFileSync(path.resolve(dist, file))
-            const ext = path.extname(file)
-            const size = zlib.brotliCompressSync(code).length
-
-            bundles[ext] ??= 0
-            bundles[ext] += size
-          }
-        }
-
-        const blue = (text: string) => `\x1b[34m${text}\x1b[39m`
-        const green = (text: string) => `\x1b[32m${text}\x1b[39m`
-
-        for (const ext in bundles) {
-          console.info(`Created bundle ${blue(`dist/index${ext}`)}: ${green(`${bundles[ext]} B`)}`)
-        }
       },
     },
   ],
