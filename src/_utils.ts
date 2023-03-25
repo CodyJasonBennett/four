102309385acc3a8d7e5edf64b6716975030adcad
Cody Bennett <hi@codyb.co>
2023-03-24 23:17:58 -0500

    chore(Compiled): cleanup

--- src/_utils.ts
+++ src/_utils.ts
@@ -16,21 +16,17 @@ export const floor = Math.floor
  * A collection of disposable objects and their compiled GPU resource.
  */
 export class Compiled<K extends object, V> extends WeakMap<K, V> {
-  // @ts-ignore
-  set<T extends K & { dispose?: never }>(object: T, compiled: V): void
-  // @ts-ignore
-  set<T extends K & { dispose(): void }>(object: T, compiled: V, dispose: () => void): void
-  // @ts-ignore
-  set<T extends K & { dispose: never | (() => void) }>(object: T, compiled: V, dispose?: () => void): void {
-    super.set(object, compiled)
-
-    if (dispose) {
+  set<T extends K & { dispose?: never }>(object: T, compiled: V): this
+  set<T extends K & { dispose(): void }>(object: T, compiled: V, dispose: () => void): this
+  set(object: K, compiled: V, dispose?: () => void): this {
+    if ('dispose' in object && typeof object.dispose === 'function') {
       const prevDispose = object.dispose.bind(object)
-      object.dispose = (): void => {
-        dispose()
-        ;(object.dispose = prevDispose)()
+      object.dispose = () => {
+        dispose?.()
+        prevDispose()
         this.delete(object)
       }
     }
+    return super.set(object, compiled)
   }
 }
